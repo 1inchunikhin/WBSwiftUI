612a5c1dedec8b9ec113f5f24f5ce3bd1191871d
Nikolay <1inchunikhin@gmail.com>
2024-06-12 12:27:10 +0300

    Initial Commit

---
+++ WBSwiftUI/WBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBSwiftUIApp.swift
+//  WBSwiftUI
+//
+//  Created by Николай Чунихин on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
