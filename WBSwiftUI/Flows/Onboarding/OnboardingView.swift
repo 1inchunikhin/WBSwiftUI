298db7abcecc9c428b8466ad8531f134c91a9279
Nikolay <1inchunikhin@gmail.com>
2024-06-12 13:23:46 +0300

    Make  project structure

---
+++ WBSwiftUI/Flows/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  WBSwiftUI
+//
+//  Created by Николай Чунихин on 12.06.2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Onboarding")
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
