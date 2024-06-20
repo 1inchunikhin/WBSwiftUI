d832dc55800d5597e605691efc21f13973e36865
Nikolay <1inchunikhin@gmail.com>
2024-06-20 19:08:46 +0300

    Add searchBar

--- WBSwiftUI/Flows/Contacts/ContactsView.swift
+++ WBSwiftUI/Flows/Contacts/ContactsView.swift
@@ -8,11 +8,75 @@
 import SwiftUI
 
 struct ContactsView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+	
+	@EnvironmentObject var router: Router
+	@State private var contacts: [Contact] = Mock.contacts
+	@State private var addContactScreenIsPresented = false
+	@State private var search: String = ""
+	
+	var body: some View {
+		NavigationStack(path: $router.path) {
+			VStack {
+				
+				SearchBar(text: $search, placeholder: "Поиск")
+					.padding(.horizontal)
+					.padding(.vertical, 4)
+				Spacer()
+				
+				
+				
+			}
+			.navigationBarTitleDisplayMode(.inline)
+			.navigationBarBackButtonHidden(true)
+			.toolbar {
+				ToolbarItem(placement: .topBarLeading) {
+					Text("Контакты")
+						.subheading1()
+						.padding(.leading, 8)
+				}
+				
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Button {
+						addContactScreenIsPresented.toggle()
+					} label: {
+						Image(systemName: "plus")
+					}
+					.tint(.black)
+					.padding(.trailing, 8)
+				}
+			}
+			
+		}
+		.navigationDestination(for: Router.Route.self) { page in
+			switch page {
+			case .detailContact:
+				Text("detail contact")
+			}
+			
+		}
+		.fullScreenCover(isPresented: $addContactScreenIsPresented, content: {
+			Button {
+				addContactScreenIsPresented.toggle()
+			} label: {
+				Text("Назад")
+			}
+		})
+		
+	}
 }
 
 #Preview {
-    ContactsView()
+	ContactsView()
+		.environmentObject(Router())
+}
+
+fileprivate enum Mock {
+	static let contacts: [Contact] = [
+		.init(name: "Анастасия", lastname: "Иванова", image: "person.fil", date: Date()),
+		.init(name: "Петя", lastname: nil, image: "person.fil", date: Date()),
+		.init(name: "Маман", lastname: nil, image: "person.fil", date: Date()),
+		.init(name: "Арбуз", lastname: "Дыни", image: "person.fil", date: Date()),
+		.init(name: "Иван", lastname: "Иванов", image: nil, date: Date()),
+		.init(name: "Лиса", lastname: "Алиса", image: nil, date: Date())
+	]
 }
---
+++ WBSwiftUI/Flows/Contacts/DataLayer/Contact.swift
@@ -0,0 +1,15 @@
+//
+//  Contact.swift
+//  WBSwiftUI
+//
+//  Created by Николай Чунихин on 20.06.2024.
+//
+
+import Foundation
+
+struct Contact {
+	let name: String
+	let lastname: String?
+	let image: String?
+	let date: Date
+}
